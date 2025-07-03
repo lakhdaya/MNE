fb91a4c4297ca6c1dc15a9aa1660aaedf28c2897
yassine lakhdari <yassinelakhdari94@gmail.com>
2025-07-03 10:16:29 +0200

    add train

--- main.py
+++ main.py
@@ -9,38 +9,39 @@ from torch.utils.data import Dataset, DataLoader
 from model import EEG_LSTM
 import torch.nn as nn
 import numpy as np
-from train import train, evaluate
+from train import train_epochs
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    epochs = 5
     print(f'Using device: {device}')
 
-    # Load data
-    X_train, y_train = load_dataset('data/train')
-    print(len(X_train))
-    X_valid, y_valid = load_dataset('data/valid')
+    #load data
+    train_dataset = EEGDataset("valid","standard_1005")
+    valid_dataset = EEGDataset("valid", "standard_1005")
 
-    train_dataset = EEGDataset(X_train, y_train)
-    valid_dataset = EEGDataset(X_valid, y_valid)
+    #load loader
+    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=8, shuffle=False)
 
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    valid_loader = DataLoader(valid_dataset, batch_size=32, shuffle=False)
+    sample, _ = train_dataset[0]
+    n_channels = sample.shape[1]
+    num_classes = 3 #fixed for T0, T1 and T2
 
-    n_channels = X_train.shape[1]
-    num_classes = len(np.unique(y_train))
-
+    #actual training is just a test not the optimized one
     model = EEG_LSTM(input_size=n_channels, hidden_size=128, num_layers=2, num_classes=num_classes).to(device)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-
-    epochs = 20
-    for epoch in range(epochs):
-        train_loss, train_acc = train(model, train_loader, criterion, optimizer, device)
-        valid_loss, valid_acc = evaluate(model, valid_loader, criterion, device)
-        print(f'Epoch {epoch+1}/{epochs} - Train loss: {train_loss:.4f}, Acc: {train_acc:.4f} - Valid loss: {valid_loss:.4f}, Acc: {valid_acc:.4f}')
-
-
+    train_epochs(
+        model, 
+        train_loader, 
+        valid_loader, 
+        criterion, 
+        optimizer, 
+        device, 
+        epochs)
+    
 
 if __name__ == "__main__":
     main()
